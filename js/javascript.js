1bea99f319235ba6a376cff3f507257240f2a56e
maxiii-benitezz <maxiiibenitezz91218@hotmail.com>
2024-11-27 00:40:47 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,13 @@
+function inclusivo() 
+{ document.body.style.backgroundColor="black" ;
+  document.getElementById("sectionr").style.color="white" ;
+document.getElementById("asider").style.color="white";
+document.body.style.backgroundImage="none";
+document.getElementById("headerr").style.color="green";
+}
+
+function autor()
+{ alert("Nombre: Máximo Jesús Benitez\nTelefono:+541161908066"
+)
+
+}
